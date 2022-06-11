97d269bccbdaf95bf4e1932c5ffd93581b67b9a4
guilhermemiua <guilhermemiua@gmail.com>
2022-06-11 11:44:50 -0300

    chore: add back button

--- src/pages/country/[cca3].tsx
+++ src/pages/country/[cca3].tsx
@@ -1,4 +1,4 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Button, Icon, useColorModeValue } from '@chakra-ui/react'
 import { GetStaticPropsContext, NextPage } from 'next'
 import { ParsedUrlQuery } from 'querystring'
 import Container from '../../components/Container'
@@ -6,6 +6,8 @@ import CountryDetails from '../../components/CountryDetails'
 import { getCountries, getCountryByCCA3 } from '../../services/countries'
 import { Country } from '../../types/Country'
 import countryMapper from '../../util/mappers/countryMapper'
+import { IoArrowBackOutline } from 'react-icons/io5'
+import { useRouter } from 'next/router'
 
 type ViewCountryDetailsByCCA3Props = {
   country: Country
@@ -14,9 +16,29 @@ type ViewCountryDetailsByCCA3Props = {
 const ViewCountryDetailsByCCA3: NextPage<ViewCountryDetailsByCCA3Props> = ({
   country
 }) => {
+  const router = useRouter()
+
+  const handleBackButton = (): void => {
+    router.push('/')
+  }
+
   return (
     <Box my={10}>
       <Container margin="0 auto">
+        <Button
+          colorScheme={useColorModeValue('whiteAlpha', 'blackAlpha')}
+          leftIcon={<Icon as={IoArrowBackOutline} />}
+          mb={10}
+          color={useColorModeValue('lightText', 'darkText')}
+          fontWeight={'light'}
+          borderRadius={0}
+          bg={useColorModeValue('lightElements', 'darkElements')}
+          px={10}
+          onClick={handleBackButton}
+        >
+          Back
+        </Button>
+
         <CountryDetails country={country} />
       </Container>
     </Box>
